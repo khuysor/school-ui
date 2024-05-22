1159652046545cf05bbb68307f5d8dd5729abdd9
khuysor <huysorkheang22@gmail.com>
2024-05-22 17:58:08 +0700

    create student modal

---
+++ src/components/StudentModal.tsx
@@ -0,0 +1,36 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
+interface Prop {
+  isOpen: boolean;
+  isClose: () => void;
+}
+const StudentModal = ({ isClose, isOpen }: Prop) => {
+  return (
+    <Modal isOpen={isOpen} onClose={isClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Modal Title</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody></ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={isClose}>
+            Close
+          </Button>
+          <Button variant="ghost">Secondary Action</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default StudentModal;
