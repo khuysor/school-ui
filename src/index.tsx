b2bd267fc3b7b58763574b1ab2cc677a8cd994b8
khuysor <huysorkheang22@gmail.com>
2024-05-22 17:58:32 +0700

    using chakra and react icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import {ChakraProvider} from "@chakra-ui/react";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <ChakraProvider>
+      <BrowserRouter>
         <App />
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
